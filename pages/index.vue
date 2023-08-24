8cb54c574ed43dcdff35959dd054e1d6296330be
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-08-24 17:48:25 -0300

    style: adjust grid layout home

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="banner">
     <NuxtImg src="https://placehold.co/600x400/000000/fff/?text=Banner" class="w-screen" />
   </div>
-  <div class="grid grid-cols-2 gap-4 p-4">
+  <div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
     <NuxtImg src="https://placehold.co/600x400?text=Aula+01" class="" />
     <NuxtImg src="https://placehold.co/600x400?text=Aula+02" class="" />
     <NuxtImg src="https://placehold.co/600x400?text=Aula+03" class="" />
