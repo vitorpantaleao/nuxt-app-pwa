ffe9340521e169fa8af4a96026d5ba053f642d45
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-09-05 15:45:44 -0300

    style: adjust colors on dark mode

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-slate-900 text-center py-3 border-t border-slate-950 text-white">
+    <footer class="bg-gray-150 dark:bg-slate-900 text-center py-3 border-t border-slate-300 dark:border-slate-950 text-gray-900 dark:text-white">
         <h4>2023 - Todos os direitos reservados</h4>
     </footer>
 </template>
--- components/Navigation/MenuBar.vue
+++ components/Navigation/MenuBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <header class="w-full bg-slate-900 sticky top-0 z-40">
+    <header class="w-full bg-gray-100 dark:bg-slate-900 sticky top-0 z-40">
         <div class="flex items-center justify-center relative">
             <template v-if="!AppStore.menuOpen">
                 <PhList :size="32" class="absolute left-4 cursor-pointer text-white" @click="toggleMenu" />
             </template>
             <NuxtLink to="/">
-                <NuxtImg src="img/logo.png" class="h-12 filter invert grayscale-[1] contrast-[2.5] brightness-[1.5]" />
+                <NuxtImg src="img/logo.png" class="h-12 filter dark:invert grayscale-[1] contrast-[2.5] brightness-[1.5]" />
             </NuxtLink>
         </div>
     </header>
