9993d826186dab1f34a291678e44d096e8ea2972
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-09-05 14:28:51 -0300

    feat: change the video on every page of lesson

--- pages/modulos/[modulo]/aula/[id].vue
+++ pages/modulos/[modulo]/aula/[id].vue
@@ -2,13 +2,12 @@
     <div class="w-screen md:w-auto md:flex overflow-hidden">
         <div class="main flex-1">
             <div class="sticky sm:static z-10 top-0">
-                <iframe class="w-full h-[300px] md:h-[700px]" :src="`${modulo.aulas[0].video}?si=1`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe class="w-full h-[300px] md:h-[700px]" :src="`${aula.video}?si=1`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="p-5">
                 <p>Você está na aula {{ route.params.id }} do módulo {{ route.params.modulo }}</p>
-                Aula 
-                <!-- <h1 class="text-2xl font-semibold">{{ modulo.nome }}</h1>
-                <p class="text-lg font-normal">{{ modulo.descricao }}</p> -->
+                <h1 class="text-2xl font-semibold">{{ modulo.nome }}</h1>
+                <p class="text-lg font-normal">{{ modulo.descricao }}</p>
             </div>
         </div>
         <ModulesAsideModules />
@@ -21,5 +20,6 @@ import { useModulesStore } from '@/stores/ModulesStore'
 
 const route = useRoute()
 const modulesStore = useModulesStore()
-const modulo = modulesStore.getModulo(route.params.id)
+const modulo = modulesStore.getModulo(route.params.modulo)
+const aula = modulo.aulas.find(aula => aula.id == route.params.id)
 </script>
