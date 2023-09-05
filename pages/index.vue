261ad4d1fe8feac5d821cf0e5eed51a9c6ecceaa
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-09-05 14:29:34 -0300

    feat: change video on page lesson

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
     <div class="w-screen md:w-full mx-auto px-4 md:px-12">
       <h2 class="text-4xl py-4 font-sans decoration-slate-500 text-slate-800 font-semibold">Módulos</h2>
       <ModulesListModules />
-      <div>
+      <div class="mb-10">
         <picture>
           <source srcset="https://placehold.co/768x250/77c4ff/fff/?text=Mobile" media="(max-width: 768px)" />
           <NuxtImg src="https://placehold.co/1024x200/77c4ff/fff?text=Desktop" class="w-screen" loading="lazy" />
