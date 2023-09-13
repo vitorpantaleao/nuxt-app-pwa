499916ecac84dde0eae11e5c21687fbd58be3b5a
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-09-13 17:49:05 -0300

    fix: fix dark color for menu icon

--- components/Navigation/MenuBar.vue
+++ components/Navigation/MenuBar.vue
@@ -2,7 +2,7 @@
     <header class="w-full bg-gray-100 dark:bg-slate-900 sticky top-0 z-40">
         <div class="flex items-center justify-center relative">
             <template v-if="!AppStore.menuOpen">
-                <PhList :size="32" class="absolute left-4 cursor-pointer text-white" @click="toggleMenu" />
+                <PhList :size="32" class="absolute left-4 cursor-pointer dark:text-white text-slate-900" @click="toggleMenu" />
             </template>
             <NuxtLink to="/">
                 <NuxtImg src="img/logo.png" class="h-12 filter dark:invert grayscale-[1] contrast-[2.5] brightness-[1.5]" />
