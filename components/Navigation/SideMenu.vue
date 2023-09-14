6536d5be888f1a75201aed8db3992279ec5c67f3
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-09-14 09:57:44 -0300

    feat: dinamic menu items

--- components/Navigation/SideMenu.vue
+++ components/Navigation/SideMenu.vue
@@ -1,67 +1,82 @@
 <template>
     <aside :class="['top-0 z-50 max-h-screen h-screen fixed md:sticky px-3 py-4 bg-gray-50 dark:bg-slate-800 transition-all duration-300 ease-out', {'w-64': menuOpen, '-translate-x-full sm:translate-x-0 md:w-fit': !menuOpen}]">
         <button @click="toggleMenu" class="absolute left-[104%] top-2 z-10" v-show="menuOpen">
-            <ph-x :size="32" class="text-black dark:text-white" />
+            <Icon icon="ic:sharp-close" width="35" class="text-black dark:text-white" />
         </button>
         <ul class="space-y-2 font-medium">
-            <li>
-                <NuxtLink to="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                    </svg>
-                    <span :class="['ml-3', {'md:hidden': !menuOpen, 'block': menuOpen}]">Dashboard</span>
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/modulos" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-                    </svg>
-                    <span :class="['flex-1 ml-3 whitespace-nowrap', {'md:hidden': !menuOpen, 'block': menuOpen}]">Módulos</span>
-                </NuxtLink>
-            </li>
-            <li>
-                <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example" @click="openSubMenu('dropdown-example')">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
-                        <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
-                    </svg>
-                    <span :class="['flex-1 ml-3 text-left whitespace-nowrap', {'md:hidden': !menuOpen, 'block': menuOpen}]">Loja</span>
-                    <svg :class="['w-3 h-3', {'md:hidden': !menuOpen, 'block': menuOpen}]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                    </svg>
-                </button>
-                <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                    <li>
-                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                </svg>
-                <span :class="['flex-1 ml-3 whitespace-nowrap', {'md:hidden': !menuOpen, 'block': menuOpen}]">Avisos</span>
-                <span :class="['inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300', {'md:hidden': !menuOpen, 'block': menuOpen}]">3</span>
-                </a>
+            <li v-for="item, i in itemsMenu" :key="i">
+                <template v-if="!item.subMenu">
+                    <NuxtLink :to="item.route" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <Icon :icon="item.icon" width="25" />
+                        <span :class="['ml-3', {'md:hidden': !menuOpen, 'block': menuOpen}]">{{ item.name }}</span>
+                        <Icon icon="ic:outline-keyboard-arrow-down" width="25" v-if="item.subMenu && menuOpen" />
+                    </NuxtLink>
+                </template>
+                <template v-else>
+                    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" :aria-controls="`'dropdown-'${i}`" :data-collapse-toggle="`'dropdown-'${i}`" @click="openSubMenu(`'dropdown-'${i}`)">
+                        <Icon :icon="item.icon" width="25" />
+                        <span :class="['flex-1 ml-3 text-left whitespace-nowrap', {'md:hidden': !menuOpen, 'block': menuOpen}]">{{ item.name }}</span>
+                        <Icon icon="ic:outline-keyboard-arrow-down" width="25" v-if="menuOpen" />
+                    </button>
+                    <ul :id="`'dropdown-'${i}`" class="hidden py-2 space-y-2">
+                        <li v-for="subItem in item.subMenu" :key="subItem">
+                            <NuxtLink :to="subItem.route" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ subItem.name }}</NuxtLink>
+                        </li>
+                    </ul>
+                </template>
             </li>
         </ul>
     </aside>
 </template>
 
 <script setup>
-import { PhX } from '@phosphor-icons/vue'
+import { Icon } from '@iconify/vue'
 import { useAppStore } from "@/stores/AppStore"
 
 const AppStore = useAppStore()
 const menuOpen = computed(() => AppStore.menuOpen)
+
+const itemsMenu = [
+    {
+        name: 'Dashboard',
+        icon: 'ic:round-space-dashboard',
+        route: '/',
+        subMenu: null
+    },
+    {
+        name: 'Módulos',
+        icon: 'ic:sharp-school',
+        route: '/modulos',
+        subMenu: null
+    },
+    {
+        name: 'Loja',
+        icon: 'ic:round-shopping-cart',
+        route: '#',
+        subMenu: null
+    },
+    {
+        name: 'Minha Conta',
+        icon: 'ic:round-account-circle',
+        route: '#',
+        subMenu: [
+            {
+                name: 'Perfil',
+                route: '#'
+            },
+            {
+                name: 'Endereço',
+                route: '#'
+            },
+        ]
+    },
+    {
+        name: 'Avisos',
+        icon: 'ic:round-notifications',
+        route: '#',
+        subMenu: null
+    }
+]
 
 function toggleMenu() {
     AppStore.toggleMenu()
